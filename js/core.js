0623e300b457489bed076a7c0d82cbfb7047ef9f
Yarik <yarichekxd@gmail.com>
2024-06-24 20:08:46 +0200

    js code

--- js/core.js
+++ js/core.js
@@ -1,3 +1,15 @@
-
-let admin; 
-let name;
+ console.log("hello")
+ const characters = [
+        { name: 'Ichigo Kurosaki', bankai: 'Tensa Zangetsu' },
+        { name: 'Kisuke Urahara', bankai: 'Benihime' },
+        { name: 'Izuru Kira', bankai: 'Wabisuke' },
+        { name: 'Sosuke Aizen', bankai: 'Kyoka Suigetsu' },
+        { name: 'Ulquiorra Cifer', bankai: 'Sonído Master' }
+        
+    ]
+  const button = document.getElementById('bankaiButton');
+  button.addEventListener('click', showBankai);
+  function showBankai(){
+    characters.forEach((characters)=>console.log(characters.name +" "+ characters.bankai))
+  }
+  
